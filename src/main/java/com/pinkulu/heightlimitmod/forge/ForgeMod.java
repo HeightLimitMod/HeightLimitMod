fcb418e98f4eae9ca63148bad43cf21b867b96b0
Wyvest <45589059+wyvest@users.noreply.github.com>
2021-06-05 09:49:33 +0700

    Update EvergreenHUD
    xander moment

--- src/main/java/com/pinkulu/heightlimitmod/forge/ForgeMod.java
+++ src/main/java/com/pinkulu/heightlimitmod/forge/ForgeMod.java
@@ -1,8 +1,14 @@
 package com.pinkulu.heightlimitmod.forge;
 
+import co.uk.isxander.evergreenhud.addon.AddonManager;
 import com.pinkulu.heightlimitmod.HeightLimitMod;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = HeightLimitMod.MOD_ID, version = HeightLimitMod.MOD_VERSION, name = HeightLimitMod.MOD_NAME, dependencies = "required-after:evergreenhud", clientSideOnly = true)
 public class ForgeMod {
+    @Mod.EventHandler
+    protected void onInit(FMLInitializationEvent event) {
+        AddonManager.getInstance().registerAddon(new HeightLimitMod());
+    }
 }
