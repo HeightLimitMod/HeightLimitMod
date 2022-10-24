920d0b34b06dacd3933aad7b608e2a46d49907e2
pinkulu <56201697+pinkulu@users.noreply.github.com>
2022-10-24 19:56:44 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ repositories {
 
 dependencies {
     modRuntimeOnly("me.djtheredstoner:DevAuth-forge-legacy:1.1.0")
-    compileOnly("cc.polyfrost:oneconfig-$platform:0.1.0+")
+    compileOnly('cc.polyfrost:oneconfig-1.8.9-forge:0.1.0-alpha108')
     val loader = when {
         platform.isLegacyForge -> "launchwrapper"
         platform.isModLauncher -> "modlauncher"
