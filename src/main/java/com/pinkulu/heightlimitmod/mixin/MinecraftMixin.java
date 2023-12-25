5bae330f56061294693d39afca4e0c997713a5ba
pinkulu <pinkulumc@gmail.com>
2023-12-25 21:47:06 +0200

    rah

---
+++ src/main/java/com/pinkulu/heightlimitmod/mixin/MinecraftMixin.java
@@ -0,0 +1,8 @@
+package com.pinkulu.heightlimitmod.mixin;
+
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Minecraft.class)
+public class MinecraftMixin {
+}
